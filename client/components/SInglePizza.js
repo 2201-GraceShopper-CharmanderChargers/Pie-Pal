8a591a47da63cd9123ecc71f3a72fa583215bbea
ShreddedPork <r.ho@miami.edu>
2022-03-08 10:43:56 -0500

    css clean up

--- client/components/SInglePizza.js
+++ client/components/SInglePizza.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPizza } from '../store/singlePizza';
+import { fetchPizza } from '../store/SinglePizza';
 import { addCart } from '../store/cart';
 import Cart from './Cart';
 import { Link } from 'react-router-dom';
