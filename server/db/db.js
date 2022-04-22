f8a29d81160e547b4b4cb7bea7807c20ab4868bc
Brennan Skinner <bgskinner3@gmail.com>
2022-04-22 14:46:53 -0400

    db changed forgot to define config

--- server/db/db.js
+++ server/db/db.js
@@ -1,43 +1,14 @@
-
 const Sequelize = require('sequelize');
 const packageJson = require('../../package.json');
-
-// console.log(chalk.blue(`Opening database connection to ${packageJson.name}`));
-
-// const databaseName =
-//   packageJson.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
-// const config = {
-//   logging: false,
-// };
-
-
-
-// if (process.env.LOGGING === 'true') {
-//   delete config.logging;
-// }
-
-// //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-// if (process.env.DATABASE_URL) {
-//   config.dialectOptions = {
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   };
-// }
-
-
-
-// console.log(`Opening database connection to ${databaseName}`);
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config
- 
-// );
 
 
 const databaseName =
   packageJson.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
-let config;
+const config = {
+  logging: false,
+};
+//let config;
 
 // if (process.env.DATABASE_URL) {
 //     config.dialectOptions = {
@@ -102,3 +73,30 @@ module.exports = db;
 // if (process.env.NODE_ENV === 'test') {
 //   after('close database connection', () => db.close());
 // }
+
+// console.log(chalk.blue(`Opening database connection to ${packageJson.name}`));
+
+// const databaseName =
+//   packageJson.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
+// const config = {
+//   logging: false,
+// };
+
+// if (process.env.LOGGING === 'true') {
+//   delete config.logging;
+// }
+
+// //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+// if (process.env.DATABASE_URL) {
+//   config.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   };
+// }
+
+// console.log(`Opening database connection to ${databaseName}`);
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config
+
+// );
