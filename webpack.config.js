060ccd38c058c3ceb82f5742a781af88a3649190
Brennan Skinner <bgskinner3@gmail.com>
2022-04-22 14:57:29 -0400

    regenerator runtime added back into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: ['./client/index.js'],
+  entry: ['regenerator-runtime/runtime.js', './client/index.js'],
   output: {
     path: __dirname,
     filename: './public/bundle.js',
