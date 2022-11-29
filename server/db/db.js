d444b9e197888f5e4d5f2b3876aa92f12ce93af7
David Dunham <daviddfh@gmail.com>
2022-11-29 16:18:48 -0500

    Attempting one more time with null in the 2nd and 3rd arguments of the Sequelize constructor.

--- server/db/db.js
+++ server/db/db.js
@@ -41,8 +41,8 @@ if (process.env.DATABASE_URL) {
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  process.env.USERNAME,
-  process.env.PASSWORD,
+  null,
+  null,
   config
 );
 module.exports = db;
